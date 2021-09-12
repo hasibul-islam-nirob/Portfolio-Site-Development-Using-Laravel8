f49ab6f5a3a8f517fad77c26f3e53ca436d0930d
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2021-09-12 19:31:04 +0600

    Add Footer Section on Front View

--- user/resources/views/Home.blade.php
+++ user/resources/views/Home.blade.php
@@ -1,12 +1,13 @@
 @extends('Layout.App')
 
 @section('content')
-    @include('Components.TopBanner');
-    @include('Components.Services');
-    @include('Components.Courses');
-    @include('Components.Projects');
-    @include('Components.ClientFeedback');
-    @include('Components.Contact');
+    @include('Components.TopBanner')
+    @include('Components.Services')
+    @include('Components.Courses')
+    @include('Components.Projects')
+    @include('Components.ClientFeedback')
+    @include('Components.Contact')
+
 @endsection
 
 
--- user/resources/views/Layout/Footer.blade.php
+++ user/resources/views/Layout/Footer.blade.php
@@ -1,3 +1,5 @@
+
+@include('Components.FooterTop')
 <div class="container-fluid bg-dark text-white m-0 text-center p-3">
     <p class="rights-text  my-2 "> &copy; 2021 Hasibul Islam Nirob, Inc. All Rights Reserved. </p>
 </div>
